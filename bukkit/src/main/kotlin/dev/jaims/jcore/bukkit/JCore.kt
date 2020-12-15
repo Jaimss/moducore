3a25ab02cd9447efb3cfd8b30a40acb8e5bc08fd
jaimss <james@harrell365.com>
2020-12-15 17:39:28 -0500

    license

--- bukkit/src/main/kotlin/dev/jaims/jcore/bukkit/JCore.kt
+++ bukkit/src/main/kotlin/dev/jaims/jcore/bukkit/JCore.kt
@@ -1,3 +1,27 @@
+/*
+ * This file is a part of JCore, licensed under the MIT License.
+ *
+ * Copyright (c) 2020 James Harrell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package dev.jaims.jcore.bukkit
 
 import dev.jaims.jcore.bukkit.manager.Managers
@@ -13,22 +37,22 @@ class JCore : JavaPlugin() {
 
     // plugin startup logic
     override fun onEnable() {
-        log("&aJCore is starting...")
+        pdmDependencySetup(this)
+        log("&aJCore is starting... &2(Version: ${description.version})")
 
         // register all managers/commands/events
-        pdmDependencySetup(this)
         managers = Managers(this)
         registerCommands(this)
         registerEvents(this)
 
-        log("&aJCore enabled!")
+        log("&aJCore enabled! &2(Version: ${description.version})")
     }
 
     // plugin shutdown logic
     override fun onDisable() {
-        log("&cJCore disabling...")
+        log("&cJCore disabling... (Version: ${description.version})")
 
-        log("&cJCore disabled.")
+        log("&cJCore disabled. (Version: ${description.version})")
     }
 
 }
